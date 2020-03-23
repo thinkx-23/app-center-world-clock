616f3a43c27bbc45ee5088ec8c8c22bb84aca1bd
Thilanka Weerasinghe <thilanka@geveo.com>
2020-03-23 11:52:14 +0530

    added app center details

--- android/app/src/main/java/com/example/world_time/MainActivity.java
+++ android/app/src/main/java/com/example/world_time/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+
+  public void onCreate(){
+    AppCenter.start(getApplication(), "8969cbb9-f92b-449d-ac69-8e55923bfb85", Analytics.class, Crashes.class);
   }
 }
